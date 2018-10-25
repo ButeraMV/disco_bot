d4c927c8b4dec72bd6c3d9af12ffb92405d3e503
Boots2291 <buteramv@gmail.com>
2018-10-25 18:09:15 -0400

    remove syntax highlighting from message

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-  let helpMessage = "```css\nCommands:\n\n!gif -> Requires additional text after the command to search Giphy for a gif\n```"
+  let helpMessage = "```\nCommands:\n\n!gif -> Requires additional text after the command to search Giphy for a gif\n```"
   message.channel.send(helpMessage).catch(console.error);
 };
