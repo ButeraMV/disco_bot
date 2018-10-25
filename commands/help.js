73a88caa6907552b8917d3a7ddd5b97ade559b3e
Boots2291 <buteramv@gmail.com>
2018-10-25 18:05:47 -0400

    fix variable name

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-  let message = "```**Here are a list of commands you can use:**\n_!gif_ Requires additional text after the command to search Giphy for a gif```"
-  message.channel.send(message).catch(console.error);
+  let helpMessage = "```**Here are a list of commands you can use:**\n_!gif_ Requires additional text after the command to search Giphy for a gif```"
+  message.channel.send(helpMessage).catch(console.error);
 };
