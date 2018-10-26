22778f7ca155a216facd9368b0da82dfb48c115d
Boots2291 <buteramv@gmail.com>
2018-10-26 11:10:15 -0400

    debug ron

--- commands/ron.js
+++ commands/ron.js
@@ -3,7 +3,8 @@ const fetch = require('node-fetch');
 exports.run = (client, message, args) => {
   fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes')
     .then(function (response) {
-      message.channel.send(response[0]);
+      console.log(response)
+      // message.channel.send(response[0]);
     })
     .catch(error => console.error(error));
 };
