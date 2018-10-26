e72ca2a8d261856447784d8bf713a0cfae848a8e
Boots2291 <buteramv@gmail.com>
2018-10-26 10:27:20 -0400

    fix dice roll

--- commands/roll.js
+++ commands/roll.js
@@ -1,13 +1,4 @@
-// exports.run = (client, message, args) => {
-//   if (args === undefined) {
-//     let sides = 6
-//   } else {
-//     let sides = args
-//   }
-//   if (sides <= 20 || sides >= 6) {
-//     let result = Math.floor(Math.random() * sides) + 1;
-//     message.reply(`you got a ${result}.`).catch(console.error);
-//   } else {
-//     message.reply('please enter a number between 6 and 20.').catch(console.error);
-//   }
-// };
+exports.run = (client, message, args) => {
+  const sides = 6;
+  message.reply(`you got a ${Math.floor(Math.random() * sides) + 1}.`).catch(console.error);
+};
