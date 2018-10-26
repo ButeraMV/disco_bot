f824ab513607099a2089dc7d06e48feeeca5d067
Boots2291 <buteramv@gmail.com>
2018-10-26 10:37:45 -0400

    fix formatting of help command

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,4 @@
 exports.run = (client, message, args) => {
-  let helpMessage = "```\nCommands:\n\n!gif -> Requires additional text after the command to search Giphy for a gif\n
-                      !roll -> Roll a 6 sided die\n
-                      !roll20 -> Roll a 20 sided die```"
+  let helpMessage = "```\nCommands:\n\n!gif -> Requires additional text after the command to search Giphy for a gif\n!roll -> Roll a 6 sided die\n!roll20 -> Roll a 20 sided die```"
   message.channel.send(helpMessage).catch(console.error);
 };
