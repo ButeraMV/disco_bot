1b47761d210777b5a01a8f1c5fe15189ff323f02
Boots2291 <buteramv@gmail.com>
2018-10-26 13:19:53 -0400

    add comment for feature to implement

--- commands/mod.js
+++ commands/mod.js
@@ -2,6 +2,7 @@ exports.run = (client, message, args) => {
   const modRole = "505412861603086356"
   if (message.member.roles.has(modRole)) {
     let memberToMod = message.mentions.members.first();
+    // Need to implement check to see if member has that role
     memberToMod.addRole(modRole).catch(console.error)
     message.reply("you have modded " + memberToMod + ".")
   } else {
