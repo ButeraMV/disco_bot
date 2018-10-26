7b488bea658b30a02bee51bc57ea3ee55e1352d5
Boots2291 <buteramv@gmail.com>
2018-10-26 11:09:07 -0400

    fix ron cmd

--- commands/ron.js
+++ commands/ron.js
@@ -3,10 +3,7 @@ const fetch = require('node-fetch');
 exports.run = (client, message, args) => {
   fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes')
     .then(function (response) {
-      client.sendMessage({
-        to: channelID,
-        message: response[0]
-      })
+      message.channel.send(response[0]);
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
 };
