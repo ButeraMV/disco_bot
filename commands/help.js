11ede5c1ffbcb8dc6fa4ba4172caceacfb9498f5
Boots2291 <buteramv@gmail.com>
2018-10-26 10:31:00 -0400

    add to help command

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,6 @@
 exports.run = (client, message, args) => {
-  let helpMessage = "```\nCommands:\n\n!gif -> Requires additional text after the command to search Giphy for a gif\n```"
+  let helpMessage = "```\nCommands:\n\n!gif -> Requires additional text after the command to search Giphy for a gif\n
+                      !roll -> Roll a 6 sided die\n
+                      !roll20 -> Roll a 20 sided die```"
   message.channel.send(helpMessage).catch(console.error);
 };
