d881704d133717d5b98ffb67381d508614ef9597
Boots2291 <buteramv@gmail.com>
2018-11-01 10:23:35 -0400

    Build out events command

--- commands/events.js
+++ commands/events.js
@@ -2,33 +2,43 @@ const fetch = require('node-fetch');
 
 exports.run = (client, message, args) => {
   let upcoming = [];
+  let output = [];
   let startDate = new Date();
-  // startDate = new Date(startDate).toISOString();
   let endDate = startDate.setDate(startDate.getDate() + 1);
   endDate = new Date(endDate).toISOString();
-  let minTime = '';
-  let maxTime = '';
+  let timeMin = '';
+  let timeMax = '';
 
   if (args.length === 0) {
-    minTime = (new Date().toISOString());
-    maxTime = endDate;
+    timeMin = (new Date().toISOString());
+    timeMax = endDate;
   } else {
     console.log(args)
   }
 
-  // 2018-10-31T14:51:06-05:00
-
-  fetch(`https://www.googleapis.com/calendar/v3/calendars/otpef10f3afpfs6u3ljcu2fsa0@group.calendar.google.com/events?maxResults=5&key=${process.env.GOOGLE_KEY}`)
+  fetch(`https://www.googleapis.com/calendar/v3/calendars/otpef10f3afpfs6u3ljcu2fsa0@group.calendar.google.com/events?maxResults=5&timeMin=${timeMin}&timeMax=${timeMax}&key=${process.env.GOOGLE_KEY}`)
     .then(function (response) { return response.json(); })
     .then(function (json) {
       for (let i = 0; i < json.items.length; i++) {
-        upcoming.push(json.items[i].summary)
-      }
-      // console.log(upcoming)
-      // message.channel.send('```\n' + upcoming.join('\n\n') + '\n```');
+        upcoming.push({"summary": json.items[i].summary, "location": json.items[i].location, "startTime": Date.parse(json.items[i].start.dateTime)})
+      };
+
+      let formattedTime = '';
+
+      for (let i = 0; i < upcoming.length; i++) {
+        let timeToFormat = (json.items[i].start.dateTime).slice(11, 16)
+
+        if (timeToFormat.slice(0, 2) > 12) {
+          let hours = timeToFormat.slice(0, 2) - 12;
+          formattedTime = (hours + ':' + timeToFormat.slice(3, 5) + 'PM')
+        }
+
+        output.push([upcoming[i].summary, (json.items[i].start.dateTime).slice(5, 10).replace('-','/') + ' at ' + formattedTime, `https://www.${upcoming[i].location}`].join('\n'))
+      };
+
       message.channel.send({ embed: {
         color: 3447003,
-        description: `${upcoming.join('\n\n')}`
+        description: `${output.join('\n\n')}`
       }})
     })
     .catch(error => console.error(error))
